2678b478d5081ed7b100fe74601128eb201b85fd
leejae honey <ljh23270@naver.com>
2022-08-01 02:27:33 +0900

    paint using mouse event

--- app.js
+++ app.js
@@ -3,22 +3,35 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// ctx.rect(50, 50, 100, 100);
-// ctx.rect(150, 150, 100, 100);
-// ctx.rect(250, 250, 100, 100);
-// ctx.rect(350, 350, 100, 100);
-// ctx.fill();
-// ctx.beginPath();
-// setTimeout(() => {
-//   ctx.rect(450, 450, 100, 100);
-//   ctx.rect(550, 550, 100, 100);
-//   ctx.fillStyle = "red";
-//   ctx.fill();
-// }, 1000);
+ctx.lineWidth = 2;
 
-ctx.moveTo(400, 400);
-ctx.lineTo(500, 400);
-ctx.lineTo(500, 500);
-ctx.lineTo(400, 500);
-ctx.lineTo(400, 400);
-ctx.stroke();
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+];
+
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+function onmouseMove(event) {
+  ctx.beginPath();
+  ctx.moveTo(400, 400);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", onmouseMove);
---
+++ canvas.js
@@ -0,0 +1,39 @@
+// ctx.rect(50, 50, 100, 100);
+// ctx.rect(150, 150, 100, 100);
+// ctx.rect(250, 250, 100, 100);
+// ctx.rect(350, 350, 100, 100);
+// ctx.fill();
+// ctx.beginPath();
+// setTimeout(() => {
+//   ctx.rect(450, 450, 100, 100);
+//   ctx.rect(550, 550, 100, 100);
+//   ctx.fillStyle = "red";
+//   ctx.fill();
+// }, 1000);
+
+/* house
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
+*/
+
+/* human
+ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
+ctx.fill();
+*/
